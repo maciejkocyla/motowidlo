1f23bfcbc0b2b1af565003fbae1a1b065e75a1a3
Maciej Kocyła <maciej.kocyla@gmail.com>
2013-12-04 21:35:42 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Motowidlo::Application.config.secret_key_base = '13c135925a64ebe97152c213b45a4ea1680cf067700db07bc9bd12ea8289bf5213201a79e1999331dbff726d90da82252a0180e0e76ec11618bad02528fcc93e'
