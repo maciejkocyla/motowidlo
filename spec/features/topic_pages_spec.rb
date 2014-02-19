7cf1907a8664db77e210ebdf59d6c25ae2ad1407
Maciej Kocyła <maciej.kocyla@gmail.com>
2014-02-19 13:37:04 +0100

    finally everything  green

--- spec/features/topic_pages_spec.rb
+++ spec/features/topic_pages_spec.rb
@@ -3,18 +3,29 @@ require 'spec_helper'
 describe "creating topic and post: " do
   subject { page }
   let(:category) { FactoryGirl.create(:category) }
+  before do
+    visit new_category_topic_path( category ) 
+  end
 
   it "redirect to new topic path, " do 
-    visit new_category_topic_path( category ) 
     expect(page).to have_content('utwórz temat: ') 
-    expect(page).to have_content('pierwszy wpis') 
-    
-    it "create a topic" do 
-      before do
-        fill_in 'topic_name',     with: "sample topic"
-        fill_in 'topic_heading',  with: "some basic heading"
-      end
-      expect {click('utwórz')}.to change(Topic, :count).by(1) 
+    expect(page).to have_content('pierwszy wpis: ') 
+  end    
+  describe "create a topic" do 
+    before do
+      fill_in 'topic_name',     with: "sample topic"
+      fill_in 'topic_heading',  with: "some basic heading"
+    end
+    it "should create topic" do 
+      expect { click_button('utwórz')}.to change(Topic, :count).by(1) 
+    end
+
+    describe " and to create a post " do
+      before { click_button('utwórz') }
+
+      it { should have_content('sample topic') }
+      it { should have_content('twoja odpowiedź') }
+      it { should have_button("dodaj odpowiedź") }
     end
   end
 end
--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -33,15 +33,15 @@ describe "user pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com')}
+        it { should have_content('user@example.com') }
+        it { should have_link('wyloguj')}
+      end
+
     end
-
-  describe "after saving the user" do
-    before { click submit }
-    let(:user) { User.find_by(email: 'user@example.com')}
-    it { should have_content('user@example.com') }
-    it { should have_link('wyloguj')}
-  end
-
   end
 
 end
