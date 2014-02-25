78e96e1c6040e4a835ae583f5f69c2c336e64cc4
Maciej Kocyła <maciej.kocyla@gmail.com>
2014-02-25 19:55:29 +0100

    categories code optimized

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,2 +1,20 @@
 module TopicsHelper
+
+  def last_topics(count)
+    topics = Topic.all
+    lastTopics = Array.new
+    if topics.count >= count
+      for t in topics[-5..-1] do 
+        lastTopics.push(t)
+      end
+    elsif topics.count < cound && topics.any? 
+      for t in topics.all do 
+        lastTopics.push(t)
+      end
+    else return 0
+    end
+
+    return lastTopics
+
+  end
 end
